a1952b4464db5ca10866fb28a02c0d42eb607fb2
Lakshay Verma <lakshayverma2610@gmail.com>
2026-09-21 18:43:56 +0530

    Fix conflicting Modifier import in MemoryGalleryScreen

--- app/src/main/java/com/nercare/cogcare/presentation/profile/MemoryGalleryScreen.kt
+++ app/src/main/java/com/nercare/cogcare/presentation/profile/MemoryGalleryScreen.kt
@@ -21,8 +21,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.clickable
 import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
